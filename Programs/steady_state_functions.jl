27cfaff1004de056c2527cac23cb152186dff70d
Mario Silva <msilva913@gmail.com>
2023-10-31 17:19:08 +0800

    include calibration with depreciation rate

--- Programs/steady_state_functions.jl
+++ Programs/steady_state_functions.jl
@@ -111,6 +111,9 @@ end
 function calibrate(targets, Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1.0)
     # Γ: gross markup
     # Ψ: elasticity of matching probability of firm locations wrt aggregate spending
+    # var_share: share of variable labor
+    #ψ: inverse of the Frisch elasticity
+    #σ: inverse of IES
         # Γ = A^α_2*(α_2*ϕ+ρ*(1-ϕ))
         # Ψ = ((1+η)/ϕ + 1-ρ)^{-1}
 
@@ -122,7 +125,7 @@ function calibrate(targets, Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1
     β = 1/(1+r)
     #TFP constant (adjustment due to composition)
     
-
+    # Altenative: do not estimate η, but use it to pin down depreciation rate target
     function loss(x)
         # Discrepancy with Ψ after checking consistency with
         #1) labor share
@@ -162,6 +165,7 @@ function calibrate(targets, Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1
     @assert abs( 1/Ψ -  ((1+η)/ϕ + 1-ρ)) < crit
 
     ϕ_C = 1.0 - ϕ_I
+    # share of workers shopping for investment goods
     ϕ_LK = (ρ-1)*δ_K*α/(r+δ_K)/((ρ-1)*δ_K*α/(r+δ_K)+1-α)
 
     # Labor types
@@ -209,6 +213,105 @@ function calibrate(targets, Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1
     @assert abs(χ*L^ψ - u_C*w/P_C) < crit
     return (A=A, β=β, ϕ=ϕ, ρ=ρ, δ_K=δ_K, α_1=α_1, α_2=α_2, α=α, ZC=ZC, ZI=ZI, χ=χ, κ=κ, ζ=ζ, η=η, σ_b=σ_b, σ=σ, ψ=ψ)
 end
+
+#Uses depreciation rate as additional target to pin down η
+function calibrate_dep(targets, Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1.0, dep_rate_ann=0.173)
+    # Γ: gross markup
+    # Ψ: elasticity of matching probability of firm locations wrt aggregate spending
+    # var_share: share of variable labor
+    #ψ: inverse of the Frisch elasticity
+    #σ: inverse of IES
+        # Γ = A^α_2*(α_2*ϕ+ρ*(1-ϕ))
+        # Ψ = ((1+η)/ϕ + 1-ρ)^{-1}
+
+    crit = 1e-10
+
+    @unpack r_annual, ϕ_I, wL_Y, occupancy_rate, Y, qC, qI, L = targets
+    A = occupancy_rate 
+    r= (1+r_annual)^(1/4) - 1
+    β = 1/(1+r)
+    δ_K = dep_rate_ann/4
+    #TFP constant (adjustment due to composition)
+    # Consistency with 
+    #1) labor share
+    #2) investment share
+    #3) markup
+    σ_b = r+δ_K
+    #1) Consistency of α with investment share
+    #ϕ_I = δ_K*α_K/(r_K*Γ)
+    α =ϕ_I*(r+δ_K)*Γ/δ_K
+
+    #2) Consistency of labor share with ρ
+    #wL_Y = (1/Γ)*(1-α+(ρ-1)*δ_K*α_K/r_K)
+    ρ =1.0 + (r+δ_K)/δ_K + (Γ*wL_Y-1)*(r+δ_K)/(α*δ_K)
+    # consistency
+    #α = (1-Γ*wL_Y)*(r+δ_K)/((r+δ_K)-(ρ-1)*δ_K)
+    α_2 = var_share*(1-α)
+
+    #3) Use Γ to extract ϕ 
+    # Γ = A^α_2*(α_2*ϕ+ρ*(1-ϕ))
+    ϕ = (ρ -Γ/A^α_2)/(ρ-α_2)
+
+    # 4) use Ψ to get η
+    η = ϕ*(1/Ψ+ρ-1) - 1
+    α_1 = 1-α-α_2
+    cons = α_1^α_1*α_2^(α_2)/((1-α)^(1-α))
+    r_K = r + δ_K
+    @assert abs(  ϕ_I - δ_K*α/(r_K*Γ) ) < crit
+    @assert abs(wL_Y- (1/Γ)*(1-α+(ρ-1)*δ_K*α/r_K)) < crit
+    @assert abs( Γ - A^α_2*(α_2*ϕ+ρ*(1-ϕ))) < crit
+    @assert abs( 1/Ψ -  ((1+η)/ϕ + 1-ρ)) < crit
+
+    ϕ_C = 1.0 - ϕ_I
+    # share of workers shopping for investment goods
+    ϕ_LK = (ρ-1)*δ_K*α/(r+δ_K)/((ρ-1)*δ_K*α/(r+δ_K)+1-α)
+
+    # Labor types
+    LC = ϕ_C*(1-ϕ_LK)*L
+    LI = ϕ_I*(1-ϕ_LK)*L
+    LK = ϕ_LK*L
+
+    I = ϕ_I*Y
+    C = ϕ_C*Y
+    K = I/δ_K
+    KC = ϕ_C*K
+    KI = ϕ_I*K
+
+    # Investment goods price 
+    p_I = 1/A^(1-ρ)
+    # Solve for Z_I to be consistent with production function
+    ZI = I/(A*p_I*KI^(α)*LI^(1-α)*cons)
+    ZC = ZI
+
+    # Solve for p_C to be consistent with consumption production function
+    p_C = C/(A*ZC*KC^(α)*LC^(1-α)*cons)
+
+    #p_C = p_I*ZI/ZC
+    P_C = A^(1-ρ)*p_C
+    # consumption bundle
+    c_A = C/P_C
+    # Implied shopping effort cost from optimality condition
+    κ = (ρ-1)*c_A/(qC^(1+η))
+    # Marginal utility imposing GHH
+    u_C = ((2+η-ρ)/(1+η)*c_A)^(-σ)
+    # consumption marginal utility
+    λ = u_C/P_C 
+
+    wL = wL_Y*Y
+    w = wL/L
+    # Level parameter for labor supply
+    χ = λ*w/L^ψ
+    # Implied efficiency of shopping for investment goods: q_I = ζ*LK
+    ζ = qI/LK
+    
+    # Elasticities 
+    # Check consistency of ζ, κ, χ
+    @assert abs(qI - ζ*LK) < crit
+    @assert abs(κ*qC^(1+η)/(ρ-1)-c_A) < crit
+    @assert abs(χ*L^ψ - u_C*w/P_C) < crit
+    return (A=A, β=β, ϕ=ϕ, ρ=ρ, δ_K=δ_K, α_1=α_1, α_2=α_2, α=α, ZC=ZC, ZI=ZI, χ=χ, κ=κ, ζ=ζ, η=η, σ_b=σ_b, σ=σ, ψ=ψ)
+end
+
 
 function table(para, ss)
     @unpack A, β, ϕ, ρ, δ_K, α_1, α_2, ZC, ZI, χ, κ, ζ = para
@@ -261,6 +364,7 @@ targets = Targets()
 para = calibrate(targets)
 ss = steady_state(para)
 tab = table(para, ss)
+
 
 
 
