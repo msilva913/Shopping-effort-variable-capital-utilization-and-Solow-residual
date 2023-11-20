2ceb747c47dc13aabf4d9efc6a3afb3c2b3d8a16
Mario Rafael Silva <msilva913@gmail.com>
2023-11-20 13:05:07 +0800

    update with investment shopping disutility

--- Programs/steady_state_functions.jl
+++ Programs/steady_state_functions.jl
@@ -9,10 +9,10 @@ using PrettyPrinting
 
 @with_kw mutable struct Targets
     r_annual::Float64 = 0.03     #associated with β
-    δ_share::Float64 = 0.173    #inapplicable unless there is DRS, fixed costs, etc.
-    ϕ_I::Float64 = 0.22          #associated with δ
+    δ_share::Float64 = 0.173    #associated with δ (can be combined with ϕ_I under fixed costs)
+    ϕ_I::Float64 = 0.22          #associated with ν
     wL_Y::Float64 = 0.667        #associated with α_L
-    occupancy_rate::Float64 = 0.78 #assoiated with A
+    occupancy_rate::Float64 = 0.78 #associated with A
     markup::Float64 = 1.3        # Combined ρ(1-ϕ)
 
     # Parameters determining units 
@@ -415,7 +415,7 @@ function calibrate_simp(targets; Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0,
                 Γ=1.3, Ψ=0.25)
 end
 
-function calibrate_only_C_fixed_costs(targets; Γ=1.3, δ_share=0.173, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1.0)
+function calibrate_fixed_costs(targets; Γ=1.3, Ψ=0.25, η=0.0, var_share=0.5, σ=2.0, ψ=1.0)
     # Γ: gross markup
     # Ψ: elasticity of matching probability of firm locations wrt aggregate spending
     # var_share: share of variable labor
@@ -428,12 +428,16 @@ function calibrate_only_C_fixed_costs(targets; Γ=1.3, δ_share=0.173, Ψ=0.25, 
 
     crit = 1e-10
 
-    @unpack r_annual, wL_Y, ϕ_I, occupancy_rate, Y, qC, qI, L = targets
+    @unpack r_annual, wL_Y, ϕ_I, δ_share, occupancy_rate, Y, qC, qI, L = targets
     A = occupancy_rate 
     r= (1+r_annual)^(1/4) - 1
     β = 1/(1+r)
     δ_K = δ_share/4
     Ψ_inv = 1/Ψ
+    ϕ_C = 1-ϕ_I
+
+    σ_b = r+δ_K
+    r_K = r+δ_K
 
     #= Labor share
     wL_Y = (1-α)/Γ*(1+ν_share)
@@ -443,23 +447,27 @@ function calibrate_only_C_fixed_costs(targets; Γ=1.3, δ_share=0.173, Ψ=0.25, 
     #α = α_ratio/(1+α_ratio)
     =#
 
+    #1) Find α
     α_ratio = ϕ_I*(r+δ_K)/(δ_K*wL_Y)
     α = α_ratio/(1+α_ratio)
 
-    #2) ν_share
-    ν_share = Γ*wL_Y/(1-α) - 1
-
-    #3) Variable labor share
+    #2)
     α_2 = var_share*(1-α)
-    #4) Use Γ to extract ϕ
 
-    A_coeff = α_2*(1+ν_share)+Ψ_inv - 1
-    B_coeff = - (Ψ_inv + Γ/A^α_2)
-    C_coeff = 1.0
+    #3) α_1 and cons
+    α_1 = 1-α-α_2
+    cons  = α_1^α_1*α_2^(α_2)/((1-α)^(1-α))
 
-    ϕ = (-B_coeff - sqrt(B_coeff^2-4*A_coeff*C_coeff))/(2*A_coeff)
+    #4) Ratio of fixed costs
+    ν_R = Γ*wL_Y/(1-α) - 1
+
+    #5) Solve for ϕ and ρ
+    A_c = α_2*(1+ν_share)+Ψ_inv - 1
+    B_c = - (Ψ_inv + Γ/A^α_2)
+    C_c = 1.0
+
+    ϕ = (-B_c - sqrt(B_c^2-4*A_c*C_c))/(2*A_c)
     ρ = 1 - Ψ_inv + 1/ϕ
-    
 
     # function loss(ϕ)
     #     ρ = 1 - Ψ_inv + 1/ϕ
@@ -468,84 +476,71 @@ function calibrate_only_C_fixed_costs(targets; Γ=1.3, δ_share=0.173, Ψ=0.25, 
 
     #  x =fzero(loss, 0.1)
 
+    #6) Calculate labor quantities
+    LC = ϕ_C*L
+    LI = ϕ_I*L
 
-    #2) Consistency of δ_K and α with investment share, markup
-    wL_Y - (1-α)/Γ*(1+ν_share)
-    ϕ_I - δ_K*α/((r+δ_K)*Γ)
-   
-
-    #TFP constant (adjustment due to composition)
-    # Consistency with 
-    #1) labor share
-    #2) investment share
-    #3) markup
-    σ_b = r+δ_K
-    r_K = r+δ_K
-    #1) Consistency of α with investment share
-    
-    α =ϕ_I*(r+δ_K)*Γ/δ_K
-
- 
-   
-
-    # 4) use Ψ to get η
-    η = ϕ*(1/Ψ+ρ-1) - 1
-    α_1 = 1-α-α_2
-    cons = α_1^α_1*α_2^(α_2)/((1-α)^(1-α))
-    r_K = r + δ_K
-    @assert abs(  ϕ_I - δ_K*α/(r_K*Γ) ) < crit
-    @assert abs(wL_Y- (1/Γ)*(1-α)) < crit
-    @assert abs( Γ - A^α_2*(α_2*ϕ+ρ*(1-ϕ))) < crit
-    @assert abs( 1/Ψ -  ((1+η)/ϕ + 1-ρ)) < crit
-
-    ϕ_C = 1.0 - ϕ_I
-    # share of workers shopping for investment goods
-    ϕ_LK = (ρ-1)*δ_K*α/(r+δ_K)/((ρ-1)*δ_K*α/(r+δ_K)+1-α)
-
-    # Labor types
-    LC = ϕ_C*(1-ϕ_LK)*L
-    LI = ϕ_I*(1-ϕ_LK)*L
-    LK = ϕ_LK*L
-
+    #7) Investment and consumption
     I = ϕ_I*Y
-    C = ϕ_C*Y
+    C = Y-I
+
+    #8) Total capital and shares 
     K = I/δ_K
     KC = ϕ_C*K
     KI = ϕ_I*K
 
-    # Investment goods price 
-    p_I = 1/A^(1-ρ)
-    # Solve for Z_I to be consistent with production function
+    #9) Investment goods price
+    p_I = 1/(A^(1-ρ))
+
+    #10) Consumption goods price (sum normalized to 1)
+    p_C = 1-p_I
+
+    #11) Solve Z_C, ZI given production functions
     ZI = I/(A*p_I*KI^(α)*LI^(1-α)*cons)
-    ZC = ZI
+    ZC = C/(A*p_C*KC^α*LI^(1-α)*cons)
 
-    # Solve for p_C to be consistent with consumption production function
-    p_C = C/(A*ZC*KC^(α)*LC^(1-α)*cons)
-
-    #p_C = p_I*ZI/ZC
+    #12) Consumption price index P_C
     P_C = A^(1-ρ)*p_C
-    # consumption bundle
-    c_A = C/P_C
-    # Implied shopping effort cost from optimality condition
-    κ = (ρ-1)*c_A/(qC^(1+η))
-    # Marginal utility imposing GHH
-    u_C = ((2+η-ρ)/(1+η)*c_A)^(-σ)
-    # consumption marginal utility
-    λ = u_C/P_C 
 
-    wL = wL_Y*Y
-    w = wL/L
-    # Level parameter for labor supply
+    #13) Consumption bundle
+    c_A = C/P_C
+
+    #14) Shopping effort
+    κ = (ρ-1)*c_A/(qC)
+
+    #15) Consumption marginal utility
+    block = c_A*(1-(ρ-1)*(1+ϕ_I/ϕ_C))
+    u_C = block^(-σ)
+
+    #16) Multiplier λ
+    λ = u_C/P_C
+
+    #17) Wage
+    w = wL_Y*Y/L
+
+    #18) Labor supply level parameter χ
     χ = λ*w/L^ψ
-    # Implied efficiency of shopping for investment goods: q_I = ζ*LK
-    ζ = qI/LK
-    
-    # Elasticities 
+
+    #19) Investment-shopping-specific disutility ζ
+    ζ = (ρ-1)/κ*(I/P_C)
+
+    #20) Fixed cost ν 
+    ν = Y/((p_I+p_C)*A)*ν_R
+
+
+
+    # Consistency of Γ, α, ϕ, ν, ρ
+    @assert abs( Γ - A^α_2*(α_2*(1+ν_R)*ϕ+ρ*(1-ϕ))) < crit
+    @assert abs( ϕ_I - δ_K*α/(r_K*Γ)*(1+ν_R) ) < crit
+    @assert abs( 1/Ψ -  (1/ϕ + 1-ρ)) < crit
+    @assert abs(wL_Y- (1/Γ)*(1-α)*(1+ν_R)) < crit
+    @assert abs(ν-Y/((p_I+p_C)*A)*ν_R) < crit
+
     # Check consistency of ζ, κ, χ
-    @assert abs(qI - ζ*LK) < crit
-    @assert abs(κ*qC^(1+η)/(ρ-1)-c_A) < crit
+    @assert abs(κ*qC/(ρ-1)-c_A) < crit
+    @assert abs(qI -(ρ-1)/(ζ*κ)*I/P_C) < crit
     @assert abs(χ*L^ψ - u_C*w/P_C) < crit
-    return (A=A, β=β, ϕ=ϕ, ρ=ρ, δ_K=δ_K, α_1=α_1, α_2=α_2, α=α, ZC=ZC, ZI=ZI, χ=χ, κ=κ, ζ=ζ, η=η, σ_b=σ_b, σ=σ, ψ=ψ,
+    return (A=A, β=β, ϕ=ϕ, ρ=ρ, δ_K=δ_K, α_1=α_1, α_2=α_2, α=α, ZC=ZC, ZI=ZI, χ=χ, κ=κ, ζ=ζ, σ_b=σ_b, σ=σ, ψ=ψ,
                 Γ=1.3, Ψ=0.25)
 end
 
