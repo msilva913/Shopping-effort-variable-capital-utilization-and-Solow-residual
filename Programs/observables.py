7ec9fdeef9389a2c6a721ba5a792434d39fe21fd
Mario Rafael Silva <msilva913@gmail.com>
2025-07-23 11:28:39 +0800

    Clean and simplify observables.py

--- Programs/observables.py
+++ Programs/observables.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 import statsmodels.api as sm
 #import statsmodels.api as sm
-from itertools import cycle as cycler
 from fredapi import Fred
 import pickle
 fred = Fred(api_key = 'd35aabd7dc07cd94481af3d1e2f0ecf3	')
@@ -13,7 +12,7 @@ fred = Fred(api_key = 'd35aabd7dc07cd94481af3d1e2f0ecf3	')
 pd.set_option('display.precision', 2)
 #pd.options.display.float_format = '{:5,.4g}'.format
 
-from time_series_functions import (moments, filter_transform, crosscorr)
+from time_series_functions import (moments, filter_transform)
 #import statsmodels
 #arima =  statsmodels.tsa.x13.x13_arima_analysis
 #path = r'C:\Users\TJSEM\Dropbox\Documents - Copy\Research\Consumption diversity, entry, and goods market frictions\Programs\Estimation'
@@ -109,22 +108,7 @@ def construct_data(init, final, freq):
     I = PCDG + PNFI + PRFI
     np.mean(PCDG/I)
     " Price index of consumption goods "
-    # non-durables
-    #p_ND = fred.get_series('DNDGRG3Q086SBEA').resample(freq).mean().dropna()
-    # Services 
-    #p_S = fred.get_series("DSERRG3Q086SBEA").resample(freq).mean().dropna()
-    # Combine using Tornquist index
-    #p_C = p_S*C_S/C + p_ND*C_ND/C
     p_C = fred.get_series("PCEPI").resample(freq).mean().dropna()
-    # p_C_alt = fred.get_series("PCEPI").resample(freq).mean().dropna()
-    # plt.plot(p_C.pct_change(), label="weighted measure")
-    # plt.plot(p_C_alt.pct_change(), label="import")
-    # plt.legend()
-    #p = pd.concat([p_S, p_ND], axis=1)
-    #q = pd.concat([C_S, C_ND], axis=1)
-  
-    #p_C = Fisher_index(p, q)
-    #C = fred.get_series('PCEC').resample(freq).mean()# monthly, nominal
     
     " Price index for investment goods "
     #p_I = fred.get_series("A006RD3Q086SBEA").resample(freq).mean().dropna()
@@ -151,30 +135,8 @@ def construct_data(init, final, freq):
     SR = tfp.dtfp
     SR_util = tfp.dtfp_util
     
-    " Relative price of investment goods "
-    #p_I = fred.get_series('PIRIC').resample('Q').mean()
-  
-    #p_C = fred.get_series("DPCERD3Q086SBEA").resample(freq).mean().dropna()
-    #Personal consumption expenditures: services
-    
-    " Labor productivity for consumption and investment"
-    #lab_prod = (C/p_C+I/p_I)/(L)
-    #lab_prod = fred.get_series("OPHNFB").resample(freq).mean().dropna()
-  
-    
-    " Labor share "
-    # Real GDP: 
-    # Y_agg = fred.get_series("GDP").resample(freq).mean().dropna()
-    # #Y_agg = Y_agg/deflator
-    # #non-farm weekly hours: BLS PRS85006023
-    # H = fred.get_series("PRS85006023").resample(freq).mean().dropna()
-    # w = fred.get_series('CES0500000003').resample(freq).mean().dropna()
-    # w/(Y_agg/H)
 
     " Scale by population and price index "
-    c_ND = C_ND/(pop*p_C)
-    c_S = C_S/(pop*p_C)
-    
     c = C/(pop*p_C)
     i = I/(pop*p_I)
     
@@ -183,10 +145,6 @@ def construct_data(init, final, freq):
     y = Y/(pop*deflator)
     " Construct output from consumption and investment "
     
-    " Wages "
-    w = fred.get_series("COMPNFB").resample(freq).mean().dropna()
-    w = w/p_C
-    #y = c + i
     lc = LC/pop
     li = LI/pop
     l = L/pop
@@ -203,7 +161,7 @@ def construct_data(init, final, freq):
   
     " Note: these series imply labor productivity in each sector "
     " List of data series "
-    var_load_list = [y, c, i, lc, li, l, lab_prod, p_I, SR, SR_util, util, util_D, util_ND, w, c_ND, c_S] 
+    var_load_list = [y, c, i, lc, li, l, lab_prod, p_I, SR, SR_util, util, util_D, util_ND] 
     return var_load_list
 
         
@@ -217,8 +175,8 @@ if __name__ == "__main__":
     #init = '1967-01-01'
     load = False
     #filter_type = 'hamilton'
-    freq = 'Q'
-    save_observables = True
+    freq = 'QE'
+    save_observables = False
     
     if load:
         var_load_list = pickle.load(open("var_load_list", "rb"))
@@ -227,7 +185,7 @@ if __name__ == "__main__":
         save_object(var_load_list, 'var_load_list')
     
     dat = pd.concat(var_load_list, axis=1)
-    lab = ['Y', 'C', 'I', 'NC', 'NI', 'N', "lab_prod", 'p_I', 'SR', 'SR_util', 'util', 'util_D', 'util_ND', 'w', 'c_ND', 'c_S']
+    lab = ['Y', 'C', 'I', 'NC', 'NI', 'N', "lab_prod", 'p_I', 'SR', 'SR_util', 'util', 'util_D', 'util_ND']
     dat.columns = lab
     """
     Create cycles
@@ -249,7 +207,7 @@ if __name__ == "__main__":
         #save_object(cycle, 'cycle')
         " Save output for estimation using growth filter"
         lab_obs = ['Y_obs', 'C_obs', 'I_obs', 'NC_obs', 'NI_obs', 'N_obs',
-               'Y_N_obs', 'p_I_obs', 'SR_obs', 'SR_util_obs', 'util_obs', 'util_D_obs', 'util_ND_obs', 'w_obs']
+               'Y_N_obs', 'p_I_obs', 'SR_obs', 'SR_util_obs', 'util_obs', 'util_D_obs', 'util_ND_obs']
         dic_data = dict(zip(lab_obs, [np.asarray(cycle_growth[x]) for x in cycle_growth.columns]))
         sio.savemat('observables_sectoral.mat', dic_data)
     
@@ -290,7 +248,8 @@ if __name__ == "__main__":
     
     " Choose specific variable set "
     #cycle_red = cycle[["Y", "I", "lab_prod", "SR", "p_I", "SR_util", "cu"]]
-
+    
+    " Generate Table 1 "
     mom_data = moments(100*cycle, lab=['I', 'NI']) 
     print(mom_data.style.format(precision=2).to_latex())
     
@@ -304,29 +263,6 @@ if __name__ == "__main__":
     years = mdates.YearLocator(5, month=1)
     years_fmt = mdates.DateFormatter('%Y')
     
-    
-    " Plot "
-    def plot_cycle(cycle, comp_variable="LI"):
-        " Plots cycle for a given decomposition "
-        nvars = cycle.shape[1]
-        linestyles = ['-', '--']
-        col_cycle = cycler(cycle.columns)
-        lsty_cycler = cycler(linestyles)
-        fig = plt.figure(figsize=(14, 8))
-        next(col_cycle)
-        for n in range(nvars-1):
-            ax = fig.add_subplot(3, 2, n+1)
-            z = next(col_cycle)
-            ax.plot(cycle[comp_variable], label=comp_variable, lw=2, alpha=0.6, linestyle=next(lsty_cycler))
-            ax.plot(cycle[z], label=f"{z}", lw=2, alpha=0.6, linestyle=next(lsty_cycler))
-            ax.legend(loc='upper right')
-            ax.xaxis.set_major_locator(years)
-            #ax.set_ylim(-0.15, 0.15)
-            ax.xaxis.set_major_formatter(years_fmt)
-            ax.grid(True)
-        plt.savefig('output_comovement_simp.pdf')
-        plt.tight_layout()
-        plt.show()
         
         
     linestyle = ['-', ':', '-.']
@@ -366,9 +302,8 @@ if __name__ == "__main__":
     cycle.mean()
     
 
-   
     
-    
+    " Figure A.9"
     " Two types of Solow residual "
     fig, ax = plt.subplots(figsize=(11, 4))
     ax.plot(100*cycle.SR, linestyle[1], label="Solow residual", lw=2, alpha=0.7, color="blue")
@@ -387,19 +322,8 @@ if __name__ == "__main__":
     cycle["util_Fern"] = cycle.SR - cycle.SR_util
     
     
-    fig, ax = plt.subplots(figsize=(11, 4))
-    ax.plot(100*cycle.util_Fern, linestyle[1], label="Utilization (Fernald)", lw=2, alpha=0.6, color="blue")
-    ax.plot(100*cycle.util, linestyle[2], label="Total capacity utilization", lw=2, alpha=0.6, color="green")
-    ax.plot(100*cycle.Y, label="Real output (consumption plus investment)", lw=2, alpha=0.6, color="black")
-    ax.xaxis.set_major_locator(years)
-    ax.xaxis.set_major_formatter(years_fmt)
-    ax.set_ylabel("Percent")
-    ax.grid(True)
-    plt.tight_layout()
-    ax.legend(fontsize=11)
-    plt.savefig("utilization_series_comparison.pdf")
     
-    " For condensed slides (utilization and output jointly)"
+    " Figure 1 "
     fig, ax = plt.subplots(figsize=(11, 4))
     ax.plot(100*cycle.util_D, linestyle[1], label= "Total capacity utilization: durables", lw=2, alpha=0.7, color="blue")
     ax.plot(100*cycle.util_ND, linestyle[2], label= "Total capacity utilization: nondurables", lw=2, alpha=0.7, color="green")
@@ -419,65 +343,9 @@ if __name__ == "__main__":
     print(mom_util.to_latex())
     
     
-    " Compare with technology series "
-    smoothed_vars = sio.loadmat("smoothed_var.mat")
-    smoothed_vars = smoothed_vars['smoothed_var']
-    var_names = ['util_obs', 'SR_obs', 'tech_obs', 'D_obs', 'h_obs']
-    # Express as list of series
-    var_list = [pd.Series(smoothed_vars[key][0][0].flatten()) for key in var_names]
-    smoothed_data = pd.concat(var_list, axis=1)
-    smoothed_data.columns = var_names
-    smoothed_data.index = cycle_growth.index
-    
-    smoothed_data_mom = moments(100*smoothed_data, lab=['SR_obs'], lags=[1])
-    
-    tech_sum = np.exp(smoothed_data.tech_obs.cumsum())
-    SR_util = np.exp(cycle_growth.SR_util.cumsum())
-    tech_sum = tech_sum.pct_change(periods=4)*100
-    SR_util = SR_util.pct_change(periods=4)*100
-    
-
-    fig, ax = plt.subplots()
-    ax.plot(smoothed_data.tech_obs, label="Smoothed model-based technology series")
-    ax.plot(smoothed_data.SR_obs, label="Fernald utilization-adjusted Solow residual")
-    ax.xaxis.set_major_locator(years)
-    ax.xaxis.set_major_formatter(years_fmt)
-    ax.set_ylabel("4 Quarter Percentage Change")
-    ax.grid(True)
-    plt.tight_layout()
-    ax.legend(fontsize=11)
-    
-    # Shopping effort
-    D_sum = np.exp(smoothed_data.D_obs.cumsum())
-    D_sum = D_sum.pct_change(periods=1)*100
-    fig, ax = plt.subplots()
-    ax.plot(D_sum.loc['2004':'2019'])
+   
     
     
-    
-    " Dynamic correlations "
-    # colors = ["red", "green", "blue"]
-    # nlags = 8
-    # nleads = 8
-    # var_labels = (["SR", "Y"], ["SR_util", "Y"], ["SR", "SR_util"])
-    # ylabels = (r'$Corr(SR_t, Y_{t+\Delta})$', r'$Corr(SR_t^{util}, Y_{t+\Delta})$', r'$Corr(SR_t, SR_{t+\Delta}^{util}$')
-    # fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(18, 6))
-    # # Loop over variables
-    # for (i, var_label) in enumerate(var_labels):
-    #     rs = pd.Series([crosscorr(cycle[var_label[0]], cycle[var_label[1]], -lag) for lag in range(-nlags, nleads)])
-    #     offset = np.floor(len(rs)/2)-np.argmax(rs)
-    #     ax.axhline(y=0.0, color="black", linestyle="--")
-    #     ax.plot(rs, linestyle[i], label=ylabels[i], alpha=0.7, linewidth=2.5, color=colors[i])
-    #     ax.axvline(np.argmax(abs(rs)),linestyle='--', color=colors[i])
-    #     ax.set_xlabel(r'$\Delta$ (quarters)', fontsize=14)
-    #     #ax[i].set_ylabel(ylabels[i], fontsize=14)
-    # ax.set_xticks(range(0, len(rs)))
-    # ax.set_xticklabels(range(-nlags, nleads))
-    # ax.set_title("Dynamic correlations", fontsize=12)
-    # ax.legend(loc = "upper right", fontsize=14)
-    # ax.axvline(nlags, color="black")
-    # plt.tight_layout()
-    # plt.savefig("dynamic_correlations_SR_Y.pdf")
     
 
 
